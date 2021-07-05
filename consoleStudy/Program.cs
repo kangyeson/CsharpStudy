f918a171f4a1faee3a4398faca013f22bb797e14
kangyeson <s2019w01@e-mirim.hs.kr>
2021-07-05 23:26:03 +0900

    Start Csharp Study

---
+++ consoleStudy/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace consoleStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("Hello, C#!");
+        }
+    }
+}
